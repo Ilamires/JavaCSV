96c5ed49e8b1d5999b499d81afaa5a6965e167cd
Ilamires <v.shamraev1@yandex.ru>
2025-05-13 19:50:07 +0300

    Initial commit

---
+++ src/main/java/com/CSVRead/Main.java
@@ -0,0 +1,8 @@
+package com.CSVRead;
+import com.opencsv.CSVReader;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/com/CSVRead/Person.java
@@ -0,0 +1,4 @@
+package com.CSVRead;
+
+public class Person {
+}
